ba5ecb6e03738c88f4d77ccf817f80c5746ae95d
Samira Goudarzi <samira.goudarzi@edu.edugrade.se>
2022-03-08 14:59:35 +0100

    fixed getrepo

--- src/script.js
+++ src/script.js
@@ -2,7 +2,7 @@ async function GetRepos () {
     const response = await fetch ('https://api.github.com/users/SamiraGo/repos');
     const data = await response.json();
     
-    document.querySelector('.ProjektHämtning').innerHTML= data[0].name;
-    document.querySelector('.Projektbeskrivning').innerHTML= data[0].description;
+    document.querySelector('.ProjektHämtning').innerHTML= data[1].name;
+    document.querySelector('.Projektbeskrivning').innerHTML= data[1].description;
 }
 GetRepos();
